ddba15a617680c18f1454272dd9a7317b29357c6
timkingteam <timkingteam@gmail.com>
2023-03-23 19:59:09 +0100

    2.3 progress indicator

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@ fn main() {
     println!("P3\n{} {}\n255", image_width, image_height);
     let b = 63;
     for r in 0..=(image_width - 1) {
+        eprintln!("lines remaining: {}", image_width - r);
         for g in 0..=(image_height - 1) {
             println!("{} {} {}", r, g, b);
         }
     }
+    eprintln!("done!");
 }
