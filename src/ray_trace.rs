875db2728a5d68bfd59bcb7040f37d39f62f17a1
timkingteam <timkingteam@gmail.com>
2023-03-26 09:45:24 +0200

    discriminant quarter

--- src/ray_trace.rs
+++ src/ray_trace.rs
@@ -5,11 +5,11 @@ use crate::vec3::*;
 pub fn hit_sphere(s: &Sphere, r: &Ray) -> Option<f64> {
     let oc = r.origin() - s.center();
     let a = dot(r.direction(), r.direction());
-    let b = 2.0 * dot(oc, r.direction());
+    let half_b = dot(oc, r.direction());
     let c = dot(oc, oc) - s.radius() * s.radius();
-    let discriminant = b * b - 4.0 * a * c;
-    if discriminant < 0.0 {
+    let discriminant_quarter = half_b * half_b - a * c;
+    if discriminant_quarter < 0.0 {
         return None;
     }
-    Some((-b - discriminant.sqrt()) / (2.0 * a))
+    Some((-half_b - discriminant_quarter.sqrt()) / a)
 }
